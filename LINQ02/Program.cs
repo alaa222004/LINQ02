df54095094041e60edab2d352ac651572aa520ce
alaa samer <alaasamer222004@gmail.com>
2025-08-09 12:12:32 +0300

    Q1solved

--- LINQ02/Program.cs
+++ LINQ02/Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using ASSLINQ;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using static ASSLINQ.ListGenerators;
 
 namespace LINQ02
 {
+    
+
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            #region Q1
+            //1.Get first Product out of Stock
+            var first=ProductList.First(x=>x.UnitsInStock==0);
+            Console.WriteLine(first);
+          
+            
+                                        
+            #endregion
         }
     }
 }
